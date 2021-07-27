cb1eed1700b92a682e9e2f1cb6aefcde3eef9118
Sergey Akimov <elit052911@gmail.com>
2021-07-27 23:39:53 +0300

    Reworking the parser base

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 import logging
 import re
-
+import time 
 
 
 
@@ -14,37 +14,23 @@ from config import TOKEN
 
 ############################
 #our parser code
+from selenium import webdriver
 
-import requests
-from bs4 import BeautifulSoup as bs
+website = "https://www.accuweather.com" #the site from which we will find out the weather(You can choose another one, but then the code will be different)
+browser = webdriver.Chrome()
+browser.get(website)
+search_bar = browser.find_element_by_class_name("search-input")
+search_button = browser.find_element_by_class_name("search-icon")
+perfomed_to_the_first_time = True
+page_now = "now"
 
-headers= {"user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.128 Safari/537.36 OPR/75.0.3969.282"}
-r = requests.get('https://www.gismeteo.com', headers=headers)
-soup = bs(r.text,"lxml")
-s1 = soup.find("div",class_="weather_now")
-now_weather = s1.text
-nw = s1.text.split("\n")
-nw.pop(13)
-nw.pop(34)
-
-for i in range(len(nw)):
-       print(nw[i])
 
        
-for i in range(len(nw)):
-       nw[i] = re.sub(r'\s', '', nw[i])
-now_weather = ' '.join(nw)
-
-
 
 
 
 
 ############################
-
-
-
-
 
 
 
@@ -59,19 +45,77 @@ bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
 
+def get_weather_at_this_moment():
+       global page_now
+       if page_now != "now":
+              browser.get(browser.find_elements_by_class_name("subnav-item")[0].get_attribute("href"))
+              page_now = "now"
+
+       buttons = browser.find_elements_by_class_name("subnav-item")
+       text = browser.find_element_by_class_name("lbar-panel").text
+       return text
+def get_weather_hourly():
+       global page_now
+       if page_now != "hourly":
+              browser.get(browser.find_elements_by_class_name("subnav-item")[1].get_attribute("href"))
+              page_now = "hourly"
+       elements = browser.find_elements_by_class_name("non-ad")
+       return elements
+
+
+def get_weather_daily():
+       global page_now
+       if page_now != "daily":
+              browser.get(browser.find_elements_by_class_name("subnav-item")[2].get_attribute("href"))
+              page_now = "daily"
+       elements = browser.find_elements_by_class_name("daily-forecast-card ")
+       return elements
+
+       
+def get_url_by_location(location):
+       global page_now
+       global buttons
+       global perfomed_to_the_first_time
+       if perfomed_to_the_first_time == True:
+              search_bar.send_keys(str(location))
+              search_button.click()
+       else:
+              search_bar2 =  browser.find_element_by_class_name("search-input")
+              search_button2 = browser.find_element_by_class_name("icon-search")
+              search_bar2.send_keys(str(location))
+              search_button2.click()
+       perfomed_to_the_first_time = False
+       page_now = "now"
+       
+
+
 #creating a greeting for our bot
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
-	await message.reply("Hi! I'm a weather bot!\nYou can use me to view the weather forecast without leaving Telegram ")
+       await message.reply("Hi! I'm a weather bot!\nYou can use me to view the weather forecast without leaving Telegram.\nFirst, specify your region in the 'region-country'.\n You can always change your region simply by writing a message with your region")
 
 	
-#Our main command to check weather
 @dp.message_handler(commands=['weathernow'])
-async def send_nowweather_comand(msg: types.Message):
-        await bot.send_message(msg.from_user.id,now_weather)
+async def send_now_weather_comand(msg: types.Message):
+       await bot.send_message(msg.from_user.id,get_weather_at_this_moment())
+       
+@dp.message_handler(commands=['weatherhourly'])
+async def send_now_weather_comand(msg: types.Message):
+       for elem in get_weather_hourly():
+              await bot.send_message(msg.from_user.id,elem.text)
 
+@dp.message_handler(commands=['weatherdaily'])
+async def send_now_weather_comand(msg: types.Message):
+       for elem in get_weather_daily():
+              await bot.send_message(msg.from_user.id,elem.text)
+       
 
-
+@dp.message_handler()
+async def echo(message: types.Message):
+       try:
+              get_url_by_location(message.text)
+       except:
+              print("Some problems")
 
 #create an infinite loop for constant bot operation
 if __name__ == '__main__':
